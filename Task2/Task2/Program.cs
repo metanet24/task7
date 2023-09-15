ca071f3f25189dd3f8635ae18b8273ffc226ae07
unknown <matanatna@code.edu.az>
2023-09-15 13:28:27 -0700

    added task

---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,170 @@
+﻿////////int studentAge = 25;
+////////Console.WriteLine(studentAge);
+
+//////string name1 = "xxx";
+//////string name2 = "yyy";
+
+//////string result = name1 + name2;
+
+//////Console.WriteLine(result);
+
+//////bool isOnline = true;
+
+//////Console.WriteLine( isOnline.GetType());
+
+//////string name = "metish";
+//////Console.WriteLine(name.GetType());
+
+
+
+////int age = 29;
+
+////if (age > 30)
+////{
+////    Console.WriteLine("qocasan");
+////}
+////else if ( age==29)
+////{
+////    Console.WriteLine("yaxshisan") ;
+////}
+////else
+////{
+////    Console.WriteLine("gencsen");
+////}
+
+//int number = 30;
+
+//string result = number > 30 ? "qocalirsan" : "yaxshisan";
+
+//Console.WriteLine(result);
+
+
+//bool res = number > 30 ? true : false;
+
+//Console.WriteLine(res);
+
+//if (number > 30)
+//{
+//    Console.WriteLine("qocalirsan");
+
+//}
+//else
+//{
+//    Console.WriteLine("yaxshisan");
+//}
+
+//int x = 10;
+//int y = 100;
+
+//String result = x > y ? "x is greater than y" :
+//                  x < y ? " is less than y " :
+//                  x == y ? "is equal y " : " no result ";
+
+//Console.WriteLine(result);
+
+//string name = "metish";
+
+//var result = name ?? "xxx";
+
+//Console.WriteLine(result);
+
+//string name = null;
+
+//var result = name ?? "xxx";
+
+//Console.WriteLine(result);
+
+
+
+//Console.WriteLine("add email");
+
+//string email= Console.ReadLine();
+
+//Console.WriteLine("add your password");
+
+//string password = Console.ReadLine();
+
+//if (email== "metanet@mail.ru" && password == "123")
+//{
+//    Console.WriteLine("succes login");
+//}
+//else
+//{
+//    Console.WriteLine("email or password is wrong");
+//}
+
+
+
+//int weekDay = 1;
+//switch (weekDay)
+//{
+//    case 1:
+//        Console.WriteLine("MONDAY");
+//        break;
+//        case 2:
+//        Console.WriteLine("TUESDAY");
+//        break;
+//        case 3:
+//        Console.WriteLine("WEDNSDAY");
+//        break;
+
+//        default: 
+//        Console.WriteLine("weekday not found ");
+//        break;
+//}
+
+
+//int[] nums = new int[1];
+//int[] nums2 = new int[3] { 1, 4, 5 };
+
+//int[] nums = { 3, 2, 6, 8, 9 };
+//for (int i = 0; i>nums.Length; i++)
+//{
+//    Console.WriteLine(nums[i]);
+//    break;
+//}
+
+
+//int[] nums = { 3, 2, 6, 2, 9, 2 };
+//int count = 0;
+
+
+//for (int i = 0; i<nums.Length; i++)
+//{
+//    if (nums[i] == 2)
+//    {
+//        count++;
+//    }
+//}
+//Console.WriteLine(count);
+
+//int[] nums = { 3, 2, 6, 2, 9, 2 };
+//int sum = 0;
+
+
+
+
+
+//int[] nums = { 3, 2, 6, 2, 9, 2 };
+//int count = 0;
+//int sumElem= 0; 
+//int sumIndex= 0;
+
+
+
+//for (int i = 0; i < nums.Length; i++)
+//{
+//    if (nums[i] == 2)
+//    {
+//        sumElem +=nums[i];
+//        sumIndex += i;
+//    }
+//}
+//int result= 0;
+
+//result= sumElem-sumIndex;
+
+//Console.WriteLine(result);
+
+
+
